74b0365154915ce48a5bb32a58639ed0e8060e4b
alexoviedo <oviedo.alex@gmail.com>
2017-09-13 22:39:31 -0400

    Event Log unit test first commit.

---
+++ src/com/company/EventLog.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventLog {
+
+    private List<Event> eventList = new ArrayList<Event>();
+
+
+    public EventLog() {
+        this.eventList = eventList;
+    }
+
+    public boolean addEvent(Event event) throws IllegalArgumentException {
+
+        boolean bool = false;
+       if (event.getName().equals( "" ) || event.getName().equals( null ))
+            throw new IllegalArgumentException( "Event name field is empty" );
+       if (event.getAction().equals( "" ) || event.getAction().equals( null ))
+            throw new IllegalArgumentException( "Event action field is empty" );
+
+       if(!(event.getAction().equals("Face2Face") || event.getAction().equals("PhoneCall") ||
+            event.getAction().equals("TextMessaging") || event.getAction().equals("Unknown" ) )){
+            throw new IllegalArgumentException( "Event action is not valid" );
+        }
+        if (!(event.getName().equals( "" ) || event.getName().equals( null )) &&
+                !(event.getAction().equals( "" ) || event.getAction().equals( null ))) {
+            if (!eventList.add( event )) {
+                bool = false;
+            } else bool = true;
+        }
+        return bool;
+    }
+
+    public int getNumEvents() {
+        return eventList.size();
+    }
+}
+
+
+
+
